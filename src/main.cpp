5595c87008bbb4132121866933e9aa40e113516f
Matěj Šťastný <matysta@outlook.com>
2025-05-19 14:32:25 -0700

    Lol

--- src/main.cpp
+++ src/main.cpp
@@ -21,11 +21,52 @@ vex::motor rightBackMotor(vex::PORT12, vex::gearSetting::ratio18_1, true);
 vex::motor armLeft(vex::PORT14, vex::gearSetting::ratio18_1, false);
 vex::motor armRight(vex::PORT15, vex::gearSetting::ratio18_1, true);
 
-int main() {
+vex::motor clawLeft(vex::PORT10, vex::gearSetting::ratio18_1, false);
+vex::motor clawRight(vex::PORT9, vex::gearSetting::ratio18_1, false);
 
-    armLeft.setVelocity(50, percent);
-    armLeft.setVelocity(50, percent);
+vex::distance distanceSensor(vex::PORT8);
 
+void driveForwardUntilDistance(double targetDistance) {
+    while (distanceSensor.objectDistance(vex::distanceUnits::cm) > targetDistance) {
+        leftFrontMotor.spin(vex::directionType::rev, 30, vex::velocityUnits::pct);
+        leftBackMotor.spin(vex::directionType::rev, 30, vex::velocityUnits::pct);
+        rightFrontMotor.spin(vex::directionType::rev, 30, vex::velocityUnits::pct);
+        rightBackMotor.spin(vex::directionType::rev, 30, vex::velocityUnits::pct);
+        vex::task::sleep(30);
+    }
+    leftFrontMotor.stop(vex::brakeType::brake);
+    leftBackMotor.stop(vex::brakeType::brake);
+    rightFrontMotor.stop(vex::brakeType::brake);
+    rightBackMotor.stop(vex::brakeType::brake);
+}
+
+void drive(double seconds, bool forwards) {
+    if (!forwards) {
+        leftFrontMotor.spin(vex::directionType::fwd, 30, vex::velocityUnits::pct);
+        leftBackMotor.spin(vex::directionType::fwd, 30, vex::velocityUnits::pct);
+        rightFrontMotor.spin(vex::directionType::fwd, 30, vex::velocityUnits::pct);
+        rightBackMotor.spin(vex::directionType::fwd, 30, vex::velocityUnits::pct);
+    } else {
+        leftFrontMotor.spin(vex::directionType::rev, 30, vex::velocityUnits::pct);
+        leftBackMotor.spin(vex::directionType::rev, 30, vex::velocityUnits::pct);
+        rightFrontMotor.spin(vex::directionType::rev, 30, vex::velocityUnits::pct);
+        rightBackMotor.spin(vex::directionType::rev, 30, vex::velocityUnits::pct);
+    }
+    vex::task::sleep(seconds * 1000);
+    leftFrontMotor.stop(vex::brakeType::brake);
+    leftBackMotor.stop(vex::brakeType::brake);
+    rightFrontMotor.stop(vex::brakeType::brake);
+    rightBackMotor.stop(vex::brakeType::brake);
+}
+
+void turn(int angle) {
+    leftFrontMotor.spinFor(vex::directionType::fwd, angle, vex::rotationUnits::deg, 30, vex::velocityUnits::pct, false);
+    leftBackMotor.spinFor(vex::directionType::fwd, angle, vex::rotationUnits::deg, 30, vex::velocityUnits::pct, false);
+    rightFrontMotor.spinFor(vex::directionType::rev, angle, vex::rotationUnits::deg, 30, vex::velocityUnits::pct, false);
+    rightBackMotor.spinFor(vex::directionType::rev, angle, vex::rotationUnits::deg, 30, vex::velocityUnits::pct);
+}
+
+void remoteControll() {
     while (true) {
         int leftSpeed = -Controller.Axis2.position();
         int rightSpeed = -Controller.Axis3.position();
@@ -45,12 +86,77 @@ int main() {
                 armLeft.spin(vex::directionType::rev, 50, vex::velocityUnits::pct);
                 armRight.spin(vex::directionType::rev, 50, vex::velocityUnits::pct);
             }
+        } else  if (Controller.ButtonL1.pressing()) {
+            if (!clawLeft.isSpinning() || !clawRight.isSpinning()) {
+                clawRight.spin(vex::directionType::fwd, 50, vex::velocityUnits::pct);
+                clawLeft.spin(vex::directionType::rev, 50, vex::velocityUnits::pct);
+            }
+        } else if (Controller.ButtonL2.pressing()) {
+            if (!clawLeft.isSpinning() || !clawRight.isSpinning()) {
+                clawLeft.spin(vex::directionType::fwd, 50, vex::velocityUnits::pct);
+                clawRight.spin(vex::directionType::rev, 50, vex::velocityUnits::pct);
+            }
         } else {
             armLeft.stop(vex::brakeType::hold);
             armRight.stop(vex::brakeType::hold);
+            clawLeft.stop(vex::brakeType::hold);
+            clawRight.stop(vex::brakeType::hold);
         }
-
-        // Small delay to prevent excessive CPU usage
         vex::task::sleep(20);
     }
+}
+
+void automatic() {
+    // Move the arm up
+    armLeft.spin(vex::directionType::rev, -50, vex::velocityUnits::pct);
+    armRight.spin(vex::directionType::fwd, 50, vex::velocityUnits::pct);
+    vex::task::sleep(440);
+    armLeft.stop(vex::brakeType::hold);
+    armRight.stop(vex::brakeType::hold);
+
+    turn(200);
+    drive(3, true);
+    turn(-430);
+    driveForwardUntilDistance(10);
+
+    // Close claw
+    clawLeft.spin(vex::directionType::rev, -50, vex::velocityUnits::pct);
+    clawRight.spin(vex::directionType::fwd, -50, vex::velocityUnits::pct);
+    vex::task::sleep(500);
+    clawLeft.stop(vex::brakeType::hold);
+    clawRight.stop(vex::brakeType::hold);
+
+    // Move the arm up
+    armLeft.spin(vex::directionType::rev, -50, vex::velocityUnits::pct);
+    armRight.spin(vex::directionType::fwd, 50, vex::velocityUnits::pct);
+    vex::task::sleep(500);
+    armLeft.stop(vex::brakeType::hold);
+    armRight.stop(vex::brakeType::hold);
+
+    drive(3, false);
+    turn(-580);
+    drive(2.8, true);
+
+    // Move the arm up
+    armLeft.spin(vex::directionType::fwd, -50, vex::velocityUnits::pct);
+    armRight.spin(vex::directionType::rev, 50, vex::velocityUnits::pct);
+    vex::task::sleep(700);
+    armLeft.stop(vex::brakeType::hold);
+    armRight.stop(vex::brakeType::hold);
+
+    clawLeft.spin(vex::directionType::fwd, -30, vex::velocityUnits::pct);
+    clawRight.spin(vex::directionType::rev, -30, vex::velocityUnits::pct);
+    vex::task::sleep(500);
+    clawLeft.stop(vex::brakeType::hold);
+    clawRight.stop(vex::brakeType::hold);
+
+
+}
+
+int main() {
+    armLeft.setVelocity(50, percent);
+    armLeft.setVelocity(50, percent);
+
+    // remoteControll();
+    automatic();
 }
